c844a3f797467cbc51f8c0bda8a8693768f96545
jaredami <jarstorm@imail.iu.edu>
2019-01-19 18:09:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="jumbotron">
+          <div className="wrapper">
+            <ul id="nav-list">
+              <li>About</li>
+              <li>Work</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="dark-wrapper">
+            <h1 id="intro">Hi, I'm Jared</h1>
+            <h1 id="info">Web Developer - Indianpolis, IN</h1>
+          </div>
+        </div>
       </div>
     );
   }
