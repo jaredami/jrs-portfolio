73e1131b4c28ab564ab8af9d73f7ab217276279f
jaredami <jarstorm@imail.iu.edu>
2019-01-25 13:32:23 -0500

    started adding hover effects to projects; added undelrine animation to nav

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../css/imagehover.min.css";
 
 class Work extends Component {
   render() {
@@ -7,16 +8,35 @@ class Work extends Component {
         <h1 id="work-header">My Work</h1>
         <div id="work-grid">
           <div className="project">
-            <img
-              className="project-screenshot"
-              src={require(`../images/integral.jpg`)}
-              alt="integral"
-            />
-            <img
-              className="project-screenshot"
-              src={require(`../images/f3.jpg`)}
-              alt="famil fun and fitness"
-            />
+            <figure
+              className="imghvr-shutter-out-diag-1"
+              style={{ backgroundColor: "#232323" }}
+            >
+              <img
+                className="project-screenshot"
+                src={require(`../images/integral.jpg`)}
+                alt="integral"
+              />
+              <figcaption style={{ backgroundColor: "#f5f5f5" }}>
+                Hover Content
+              </figcaption>
+            </figure>
+          </div>
+
+          <div className="project">
+            <figure
+              className="imghvr-zoom-out-flip-vert"
+              style={{ backgroundColor: "#232323" }}
+            >
+              <img
+                className="project-screenshot"
+                src={require(`../images/f3.jpg`)}
+                alt="famil fun and fitness"
+              />
+              <figcaption style={{ backgroundColor: "#f5f5f5" }}>
+                Hover Content
+              </figcaption>
+            </figure>
           </div>
         </div>
       </div>
